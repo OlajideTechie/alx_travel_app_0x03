ef0b8d52f9f02a6c2819215d85268a324de1af61
Olajide <oolajide91@gmail.com>
2025-11-05 06:11:14 +0100

    404 page not found fix

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -15,13 +15,13 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),
     path('api/auth/', include('rest_framework.urls')),
 
-
     # Swagger
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
